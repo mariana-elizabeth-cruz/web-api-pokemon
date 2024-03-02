e70c7749a78185272802c6fa10f754b0462f3b6a
mariana-elizabeth-cruz <lizzybtk@outlook.com>
2024-03-02 11:17:22 -0300

    listando pokemones

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-// import DetailPokemon from './components/DetailPokemon'
-// import Home from './components/Home'
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import ListPokemon from "./components/ListPokemon";
 
-
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" elemtnt={<Home />} />
-          <Route path="/pokemon/:id" elemtnt={<DetailPokemon />} />
-        </Routes>
-      </BrowserRouter> */}
-      <h1>pokemon</h1>
+      <ListPokemon/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
